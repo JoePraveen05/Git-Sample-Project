023bf7aca2ca0735df7e86fd31c1a32b8fcd14d6
JoePraveen05 <joepraveen05@gmail.com>
2026-02-23 11:42:20 +0530

    Master

---
+++ src/test/java/com/omrbranch/git/sampleproject/GitSampleProject.java
@@ -0,0 +1,20 @@
+package com.omrbranch.git.sampleproject;
+
+public class GitSampleProject {
+
+	public void test1() {
+		System.out.println("Master test 1");
+
+	}
+
+	public void test2() {
+		System.out.println("Master test 2");
+
+	}
+
+	public void test3() {
+		System.out.println("Master test 3");
+
+	}
+
+}
