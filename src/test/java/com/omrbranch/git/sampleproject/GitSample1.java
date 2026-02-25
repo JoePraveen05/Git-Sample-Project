de0e63356a58b401ab147089605a2c1f9ecac63d
JoePraveen05 <joepraveen05@gmail.com>
2026-02-25 10:50:00 +0530

    Tester1 Script

---
+++ src/test/java/com/omrbranch/git/sampleproject/GitSample1.java
@@ -0,0 +1,19 @@
+package com.omrbranch.git.sampleproject;
+
+public class GitSample1 {
+
+	public void test1() {
+		System.out.println("Tester1 test 1");
+
+	}
+
+	public void test2() {
+		System.out.println("Tester1 test 2");
+
+	}
+
+	public void test3() {
+		System.out.println("Tester1 test 3");
+	}
+
+}
